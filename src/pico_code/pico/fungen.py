fa38031b48bf29f70e0db41d37af6ca4f2c332d0
Romilly Cocking <romilly.cocking@gmail.com>
2021-02-08 14:11:02 +0000

    Tidied up mcp3008 code and documentation.

--- src/pico_code/pico/fungen.py
+++ src/pico_code/pico/fungen.py
@@ -1,39 +1,10 @@
 # Example of using PIO for function generation using hacked PWM example
 
-from machine import Pin, ADC
-from rp2 import PIO, StateMachine, asm_pio
+from machine import ADC
 from time import sleep_us, sleep_ms
 import _thread
 
-
-@asm_pio(sideset_init=PIO.OUT_LOW)
-def pwm_prog():
-    pull(noblock) .side(0)
-    mov(x, osr) # Keep most recent pull data stashed in X, for recycling by noblock
-    mov(y, isr) # ISR must be preloaded with PWM count max
-    label("pwmloop")
-    jmp(x_not_y, "skip")
-    nop()         .side(1)
-    label("skip")
-    jmp(y_dec, "pwmloop")
-
-
-class PIOPWM:
-    def __init__(self, sm_id, pin, max_count, count_freq):
-        self._sm = StateMachine(sm_id, pwm_prog, freq=2 * count_freq, sideset_base=Pin(pin))
-        # Use exec() to load max count into ISR
-        self._sm.put(max_count)
-        self._sm.exec("pull()")
-        self._sm.exec("mov(isr, osr)")
-        self._sm.active(1)
-        self._max_count = max_count
-
-    def set(self, value):
-        # Minimum value is -1 (completely turn off), 0 actually still produces narrow pulse
-        value = max(value, -1)
-        value = min(value, self._max_count)
-        self._sm.put(value)
-
+from pio_dac import PIOPWM
 
 # Pin GP0 (physical pin 1) is output
 pwm = PIOPWM(0, 22, max_count=250, count_freq=10_000_000)
--- src/pico_code/pico/mcp3008.py
+++ src/pico_code/pico/mcp3008.py
@@ -12,39 +12,39 @@ class MCP3008:
     """
 
 
-def __init__(self, spi, cs, ref_voltage=3.3):
-    """
-    Initialise MC3008 instance.
+    def __init__(self, spi, cs, ref_voltage=3.3):
+        """
+        Initialise MC3008 instance.
 
-    :param spi: SPI bus the ADC is connected to.
-    :param cs: Chips elect pin.
-    :param ref_voltage: Voltage into (Vin) the ADC.
-    """
-    self.cs = cs
-    self._spi_device = spi
-    self._out_buf = bytearray(3)
-    self._out_buf[0] = 0x01
-    self._in_buf = bytearray(3)
-    self._ref_voltage = ref_voltage
-
-
-def reference_voltage(self):
-    """Returns the MCP3xxx's reference voltage. (read-only)"""
-    return self._ref_voltage
+        :param spi: SPI bus the ADC is connected to.
+        :param cs: Chips elect pin.
+        :param ref_voltage: Voltage into (Vin) the ADC.
+        """
+        self.cs = cs
+        self._spi_device = spi
+        self._out_buf = bytearray(3)
+        self._out_buf[0] = 0x01
+        self._in_buf = bytearray(3)
+        self._ref_voltage = ref_voltage
 
 
-def read(self, pin, is_differential=False) -> int:
-    """read the voltage on or petween a pin or pario of pins as a scaled integer.
-    :param int pin: individual or differential pin.
-    :param bool is_differential: single-ended or differential read.
-    :return: The returned value ranges from 0 to 1023 inclusive. 0 = 0V0, 1023 = 100% of reference voltage.
+    def reference_voltage(self):
+        """Returns the MCP3xxx's reference voltage. (read-only)"""
+        return self._ref_voltage
 
-    If you want to do a differential read, the ``pin`` parameter should be the first of the two pins
-    associated with the desired differential channel mapping.
-    """
 
-    self.cs.value(0)  # select
-    self._out_buf[1] = ((not is_differential) << 7) | (pin << 4)
-    self._spi_device.write_readinto(self._out_buf, self._in_buf)
-    self.cs.value(1)  # turn off
-    return ((self._in_buf[1] & 0x03) << 8) | self._in_buf[2]
+    def read(self, pin, is_differential=False) -> int:
+        """read the voltage on or petween a pin or pario of pins as a scaled integer.
+        :param int pin: individual or differential pin.
+        :param bool is_differential: single-ended or differential read.
+        :return: The returned value ranges from 0 to 1023 inclusive. 0 = 0V0, 1023 = 100% of reference voltage.
+
+        If you want to do a differential read, the ``pin`` parameter should be the first of the two pins
+        associated with the desired differential channel mapping.
+        """
+
+        self.cs.value(0)  # select
+        self._out_buf[1] = ((not is_differential) << 7) | (pin << 4)
+        self._spi_device.write_readinto(self._out_buf, self._in_buf)
+        self.cs.value(1)  # turn off
+        return ((self._in_buf[1] & 0x03) << 8) | self._in_buf[2]
