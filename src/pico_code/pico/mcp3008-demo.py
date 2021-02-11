e6a019c8977b038a435030bc02fd2de483c1d5f1
Romilly Cocking <romilly.cocking@gmail.com>
2021-02-11 15:22:51 +0000

    Fixed mcp3008 bug

--- src/pico_code/pico/mcp3008-demo.py
+++ src/pico_code/pico/mcp3008-demo.py
@@ -1,8 +1,10 @@
 from time import sleep
 from mcp3008 import MCP3008
+from machine import Pin
 
-spi = machine.SPI(0, baudrate=100000)
+spi = machine.SPI(0, sck=Pin(2),mosi=Pin(3),miso=Pin(4), baudrate=100000)
 cs = machine.Pin(22, machine.Pin.OUT)
+
 
 chip = MCP3008(spi, cs)
 
