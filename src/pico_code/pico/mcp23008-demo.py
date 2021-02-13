37cfdbd9877abb7d1f8a75cca8e99f0e7c5eccd9
Romilly Cocking <romilly.cocking@gmail.com>
2021-02-13 09:14:55 +0000

    Trying to debug OS Error 5 with mcp23008.

--- src/pico_code/pico/mcp23008-demo.py
+++ src/pico_code/pico/mcp23008-demo.py
@@ -4,13 +4,23 @@ from machine import Pin, I2C
 
 i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=100000) # pins for Pico Explorer
 
+# Code added to help with debugging
+
+
+
 def loop():
     ic = MCP23008(i2c)
     ic.direction(0x00)
+    count = 0
     while True:
         for i in range(256):
-            ic.output(i)
-            sleep(0.5)
+            count += 1
+            try:
+                ic.output(i)
+            except:
+                print ('failed after %d iterations' % count)
+                return
+            sleep(0.1)
 
 
 loop()
