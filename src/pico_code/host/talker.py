e8f2a1d6d42b649ec40ce3664648ac1879657bf7
Romilly Cocking <romilly.cocking@gmail.com>
2022-08-07 14:34:19 +0100

    Fixed talker bug

--- src/pico_code/host/talker.py
+++ src/pico_code/host/talker.py
@@ -5,18 +5,15 @@ class Talker:
     TERMINATOR = '\r'.encode('UTF8')
 
     def __init__(self, timeout=1):
-        """
-        Initialise Talker instance
-
-
-        :param timeout:
-        """
         self.serial = serial.Serial('/dev/ttyACM0', 115200, timeout=timeout)
 
     def send(self, text: str):
         line = '%s\r\f' % text
         self.serial.write(line.encode('utf-8'))
-        return self.receive() == text # the line should be echoed, so the result should match
+        reply = self.receive()
+        reply = reply.replace('>>> ','') # lines after first will be prefixed by a propmt
+        if reply != text: # the line should be echoed, so the result should match
+            raise ValueError('expected %s got %s' % (text, reply))
 
     def receive(self) -> str:
         line = self.serial.read_until(self.TERMINATOR)
